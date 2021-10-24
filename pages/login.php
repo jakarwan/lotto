07198ffabba90e71c58c68add057d0f36e60f441
jakarwan <55525006+jakarwan@users.noreply.github.com>
2021-10-24 22:19:09 +0700

    edit login

--- pages/login.php
+++ pages/login.php
@@ -1,48 +1,6 @@
 <?php
 include '../config/config.php';
 session_start();
-
-
-if (isset($_POST['submit'])) {
-  $username = $_POST["txtUsername"];
-  $password = trim($_POST['txtPassword']);
-  // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-  #$rs = mysqli_query($conn,"SELECT * FROM users WHERE username ='".$_POST['txtUsername']."' AND password ='".$_POST['txtPassword']."'" );
-  // $sql = mysqli_query($conn,"SELECT * FROM users WHERE username ='".$_POST['txtUsername']."'" );
-  $sql = "SELECT * FROM users WHERE Username = '" . mysqli_real_escape_string($conn, $username) . "' AND password ='".mysqli_real_escape_string($conn, $password)."' ";
-  $rs = mysqli_query($conn, $sql);
-  $num = mysqli_num_rows($rs);
-
-
-  if ($num > 0) {
-    $row = mysqli_fetch_array($rs);
-    // if (password_verify($password, $row['password'])) {
-      $_SESSION["users"] = $row["username"];
-      $_SESSION["name"] = $row["user_name"];
-      $_SESSION["date"] = $row["date"];
-      $_SESSION["phone"] = $row["phone"];
-      $_SESSION["status"] = $row["status"];
-      if($row["status"] == 0){
-        $_SESSION["status"] = 'User';
-      } else if ($row["status"] == 1) {
-        $_SESSION["status"] = 'Admin';
-      } else {
-        session_destroy();
-      }
-      if ($row['image'] != null) {
-        $_SESSION["pic"] = $row["image"];
-      } else {
-        $_SESSION["pic"] = "../images/faces/face1.jpg";
-      }
-      header('refresh: 0;index.php');
-    // }
-    //  else {
-    //   echo '<script>alert("ชื่อผู้ใช้หรือรหัสผ่านผิดพลาด \nไม่สามารถเข้าระบบได้");</script>';
-    // }
-  } else {
-    echo '<script>alert("ชื่อผู้ใช้หรือรหัสผ่านผิดพลาด \nไม่สามารถเข้าระบบได้");</script>';
-  }
-}
 // if(isset($_POST['txtUsername']) && isset($_POST['txtPassword'])){
 // 	$rs = mysqli_query($conn,"SELECT * FROM users WHERE username ='".$_POST['txtUsername']."' AND password ='".$_POST['txtPassword']."'" );
 // 	$num = mysqli_num_rows($rs);
@@ -82,12 +40,56 @@ if (isset($_POST['submit'])) {
   <!-- End plugin css for this page -->
   <!-- inject:css -->
   <link rel="stylesheet" href="../css/style.css">
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <!-- endinject -->
   <link rel="shortcut icon" href="../images/favicon.png" />
 </head>
 
 <body>
   <form method="post">
+    <?php 
+    if (isset($_POST['submit'])) {
+      $username = $_POST["txtUsername"];
+      $password = trim($_POST['txtPassword']);
+      // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+      #$rs = mysqli_query($conn,"SELECT * FROM users WHERE username ='".$_POST['txtUsername']."' AND password ='".$_POST['txtPassword']."'" );
+      // $sql = mysqli_query($conn,"SELECT * FROM users WHERE username ='".$_POST['txtUsername']."'" );
+      $sql = "SELECT * FROM users WHERE Username = '" . mysqli_real_escape_string($conn, $username) . "' AND password ='".mysqli_real_escape_string($conn, $password)."' ";
+      $rs = mysqli_query($conn, $sql);
+      $num = mysqli_num_rows($rs);
+    
+    
+      if ($num > 0) {
+        $row = mysqli_fetch_array($rs);
+        // if (password_verify($password, $row['password'])) {
+          $_SESSION["users"] = $row["username"];
+          $_SESSION["name"] = $row["user_name"];
+          $_SESSION["date"] = $row["date"];
+          $_SESSION["phone"] = $row["phone"];
+          $_SESSION["status"] = $row["status"];
+          $_SESSION["userId"] = $row["user_id"];
+          if($row["status"] == 0){
+            $_SESSION["status"] = 'User';
+          } else if ($row["status"] == 1) {
+            $_SESSION["status"] = 'Admin';
+          } else {
+            session_destroy();
+          }
+          if ($row['image'] != null) {
+            $_SESSION["pic"] = $row["image"];
+          } else {
+            $_SESSION["pic"] = "../images/faces/face1.jpg";
+          }
+          header('refresh: 0;index.php');
+        // }
+        //  else {
+        //   echo '<script>alert("ชื่อผู้ใช้หรือรหัสผ่านผิดพลาด \nไม่สามารถเข้าระบบได้");</script>';
+        // }
+      } else {
+        echo '<script type="text/javascript">Swal.fire("ไม่สามารถเข้าระบบได้!","ชื่อผู้ใช้หรือรหัสผ่านผิดพลาด!","warning");</script>';
+      }
+    }
+    ?>
     <div class="container-scroller">
       <div class="container-fluid page-body-wrapper full-page-wrapper auth-page">
         <div class="content-wrapper d-flex align-items-center auth auth-bg-1 theme-one">
@@ -95,9 +97,9 @@ if (isset($_POST['submit'])) {
             <div class="col-lg-4 mx-auto">
               <div class="auto-form-wrapper">
                 <div class="form-group">
-                  <label class="label">Username</label>
+                  <label class="label">ชื่อผู้ใช้</label>
                   <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Username" id="txtUsername" name="txtUsername" required="">
+                    <input type="text" class="form-control" placeholder="ชื่อผู้ใช้" id="txtUsername" name="txtUsername" required="">
                     <div class="input-group-append">
                       <span class="input-group-text">
                         <i class="mdi mdi-check-circle-outline"></i>
@@ -106,7 +108,7 @@ if (isset($_POST['submit'])) {
                   </div>
                 </div>
                 <div class="form-group">
-                  <label class="label">Password</label>
+                  <label class="label">รหัสผ่าน</label>
                   <div class="input-group">
                     <input type="password" class="form-control" placeholder="*********" id="txtPassword" name="txtPassword" required="">
                     <div class="input-group-append">
@@ -117,23 +119,23 @@ if (isset($_POST['submit'])) {
                   </div>
                 </div>
                 <div class="form-group">
-                  <button type="submit" id="submit" name="submit" class="btn btn-primary submit-btn btn-block">Login</button>
+                  <button type="submit" id="submit" name="submit" class="btn btn-primary submit-btn btn-block">เข้าสู่ระบบ</button>
                 </div>
                 <div class="form-group d-flex justify-content-between">
                   <div class="form-check form-check-flat mt-0">
                     <label class="form-check-label">
-                      <input type="checkbox" class="form-check-input" checked> Keep me signed in
+                      <input type="checkbox" class="form-check-input" checked> จดจำรหัสผ่าน
                     </label>
                   </div>
-                  <a href="#" class="text-small forgot-password text-black">Forgot Password</a>
+                  <a href="#" class="text-small forgot-password text-black">ลืมรหัสผ่าน</a>
                 </div>
                 <!-- <div class="form-group">
                   <button class="btn btn-block g-login">
                     <img class="mr-3" src="../../images/file-icons/icon-google.svg" alt="">Log in with Google</button>
                 </div> -->
                 <div class="text-block text-center my-3">
-                  <span class="text-small font-weight-semibold">Not a member ?</span>
-                  <a href="register.php" class="text-black text-small">Create new account</a>
+                  <span class="text-small font-weight-semibold">ยังไม่มีชื่อผู้ใช้งานเพื่อเข้าสู่ระบบ ?</span>
+                  <a href="register.php" class="text-black text-small">สมัครสมาชิก</a>
                 </div>
 
               </div>
